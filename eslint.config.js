ecc91b2ceefa9d7d5a4cdcb9c22fa2bc7aa9c61c
pro-verstka <pro-verstka@yandex.ru>
2024-10-06 12:56:44 +0400

    Update eslint 8 -> 9

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import eslintConfigPrettier from 'eslint-config-prettier'
+import perfectionist from 'eslint-plugin-perfectionist'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginReact from 'eslint-plugin-react'
+import tseslint from 'typescript-eslint'
+import pluginJs from '@eslint/js'
+import globals from 'globals'
+
+export default [
+	perfectionist.configs['recommended-line-length'],
+	eslintConfigPrettier,
+	{
+		files: ['**/*.{ts,tsx}']
+	},
+	{
+		languageOptions: {
+			globals: globals.browser
+		}
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react/jsx-curly-brace-presence': [
+				'error',
+				{
+					propElementValues: 'always',
+					children: 'never',
+					props: 'never'
+				}
+			],
+			'react-refresh/only-export-components': [
+				'warn',
+				{
+					allowConstantExport: true
+				}
+			],
+			'@typescript-eslint/no-explicit-any': [
+				'warn',
+				{
+					ignoreRestArgs: true
+				}
+			],
+			'react-hooks/exhaustive-deps': 'error',
+			'perfectionist/sort-imports': 'error',
+			'no-mixed-spaces-and-tabs': 'off',
+			'react/react-in-jsx-scope': 'off',
+			'react/display-name': 'off',
+			'react/prop-types': 'off',
+			'no-extra-semi': 'warn'
+		},
+		plugins: {
+			'react-refresh': reactRefresh,
+			'react-hooks': reactHooks
+		},
+		settings: {
+			react: {
+				version: 'detect'
+			}
+		}
+	}
+]
